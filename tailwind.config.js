b2490a0f71c6e8e228d895479ca93cdbf92e4329
Ilham Malabu <malabuzainab@gmail.com>
2021-09-21 19:42:05 +0100

    redesigned whole website

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        text: "#7F7C82",
+        bg1: "#BFA2DB",
+        bg2: "#F0D9FF",
+        bg3: "#F3F1F5"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
