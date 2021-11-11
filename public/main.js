63f10ccfabbae381b0bcd8354bc3e0261372dc9c
Ilham Malabu <malabuzainab@gmail.com>
2021-11-11 17:57:33 +0100

    organized

---
+++ public/main.js
@@ -0,0 +1,11 @@
+document.addEventListener(getRandom())
+
+function getRandom() {
+    fetch("https://motivational-quote-api.herokuapp.com/quotes/random")
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById("quote").innerHTML = data.quote
+            document.getElementById("person").innerHTML = data.person
+        })
+        .catch((err) => console.log(err))
+}
