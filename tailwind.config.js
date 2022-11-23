f7b34e00ba93896d1b8f39a1b5e18ce1877bdaee
zainabumalabu <malabuzainab@gmail.com>
2022-11-23 22:20:09 +0100

    tailwind fix?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 module.exports = {
   purge: {
     node: 'layers',
-    content: [
-      "./node_modules/flowbite/**/*.js"
-  ]
+    content: ["./src/**/*.{html,js}"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       colors: {
         'c-yellow': '#FDFFD4',
