6eecf3590fabd217320d1d14c39c76b0f4e704f6
Zainabu <malabuzainab@gmail.com>
2025-03-16 15:58:53 +0100

    edit

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
-document.addEventListener(getRandom());
-document.addEventListener(changeColor());
+// Fixing bugs and improving mobile responsiveness
 
-function getRandom() {
-    fetch("https://motivational-quotes-api.onrender.com/quotes/random")
-        .then((response) => response.json())
-        .then((data) => {
-            document.getElementById("quote").innerHTML = data.quote
-            document.getElementById("person").innerHTML = data.person
-        })
-        .catch((err) => console.log(err))
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.getElementById("navbar");
+    const menuButton = document.getElementById("menu-button");
+    
+    // if (menuButton && navbar) {
+    //     menuButton.addEventListener("click", () => {
+    //         navbar.classList.toggle("hidden");
+    //         navbar.classList.toggle("flex");
+    //     });
+    // }
 
+    // // Ensure images and containers resize properly
+    // document.querySelectorAll("img").forEach(img => {
+    //     img.classList.add("max-w-full", "h-auto");
+    // });
 
-// let names = document.getElementById("name").className;
-// let c1 = '#E8D7FF';
-// let c2 = '#FFD3E8';
-// let c3 = '#F39C91';
-// let c4 = '#A5C694';
-// let f = false;
+    // document.querySelectorAll(".responsive-container").forEach(container => {
+    //     container.classList.add("w-full", "px-4", "md:px-8", "lg:px-12");
+    // });
 
-// function changeColor() {
-//     setInterval(function () {
-//         if (f) {
-//             names = c1.value()
-//         };
-//         f = !f;
-//     }, 500);
-// };
+    // Fix getRandom function to load quotes correctly
+    function getRandom() {
+        fetch("https://motivational-quotes-api.onrender.com/quotes/random")
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById("quote").innerHTML = data.quote;
+                document.getElementById("person").innerHTML = data.person;
+            })
+            .catch(err => console.log("Error fetching quote:", err));
+    }
+
+    document.getElementById("quote-button")?.addEventListener("click", getRandom);
+    getRandom(); // Fetch a quote on page load
+
+    // Improve color change function
+    function changeColor() {
+        let colors = ["#E8D7FF", "#FFD3E8", "#F39C91", "#A5C694"];
+        let index = 0;
+        setInterval(() => {
+            document.body.style.backgroundColor = colors[index];
+            index = (index + 1) % colors.length;
+        }, 2000);
+    }
+
+    //changeColor(); // Start background color change on load
+});
